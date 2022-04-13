52ff88f858677480f74745bcf2655e7eaa3c287a
Vic_Rain <44798254+LiVicRain@users.noreply.github.com>
2022-04-13 17:08:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//요소 선택 및 배열 선택
+const chatList = document.getElementById('chat_list');
+const chatForm = document.getElementById('chat_form')
+let chatArr = [];
+
+//displayChat 함수
+function displayChat(){
+    chatList.innerHTML = ""
+}
